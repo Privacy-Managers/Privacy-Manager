16c1e3c2387c9813fa97fcd602c1f416c52c75c1
Manvel Saroyan <saroyanm@gmail.com>
2018-09-23 22:17:51 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-zip -r privacy-manager.zip . -x *.git* -x README.md  -x .gitignore -x COPYING -x build.sh
+zip -r privacy-manager.zip . -x *.git* -x README.md  -x .gitignore -x COPYING -x build.sh -x .DS_Store
