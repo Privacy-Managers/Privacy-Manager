921947e01384e23df1489060b2ba722188469189
Manvel Saroyan <saroyanm@gmail.com>
2019-05-09 16:21:04 +0200

    Updated the license header with present

--- src/js/ui/common.js
+++ src/js/ui/common.js
@@ -1,6 +1,6 @@
 /*
  * This file is part of Privacy Manager.
- * Copyright (C) 2017 Manvel Saroyan
+ * Copyright (C) 2017-present Manvel Saroyan
  * 
  * Privacy Manager is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
