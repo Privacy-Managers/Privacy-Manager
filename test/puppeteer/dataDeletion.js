a9840b152be11f6f8660781f713f25727e5b1f97
Manvel Saroyan <saroyanm@gmail.com>
2019-12-19 15:24:06 +0700

    Noissue - Reorganized icon files and added missing GPL headers to the files

--- test/puppeteer/dataDeletion.js
+++ test/puppeteer/dataDeletion.js
@@ -1,8 +1,24 @@
+/*
+ * This file is part of Privacy Manager.
+ * Copyright (C) 2017-present Manvel Saroyan
+ * 
+ * Privacy Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Privacy Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Privacy Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const {allUrlsToPermissions, restorePermissions} = require("../manifest");
 const {openPopupPage, closeBrowser, getBrowser} = require("../common");
 const {equal, ok} = require("assert");
-const {readFileSync} = require("fs");
-const {join} = require("path");
 
 let page;
 let page2;
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,21 @@
+/*
+ * This file is part of Privacy Manager.
+ * Copyright (C) 2017-present Manvel Saroyan
+ * 
+ * Privacy Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Privacy Manager is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Privacy Manager. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 const path = require("path");
 const argv = require("minimist")(process.argv.slice(2));
 const CopyPlugin = require('copy-webpack-plugin');
@@ -21,6 +39,7 @@ module.exports =
     new CopyPlugin([
       { from: './src/_locales', to: "_locales" },
       { from: './src/css', to: "css" },
+      { from: './src/img', to: "img" },
       { flatten: true, from: './src/*'},
       {from: "node_modules/webextension-polyfill/dist/browser-polyfill.min.js",
        to: "js"}
